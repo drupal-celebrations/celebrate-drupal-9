fc247913ee82fc772a780106ce23d0cee09fd7d6
Dan Lemon <daniel.lemon@amazee.com>
2020-05-12 13:02:58 +0200

    Taildwind screen/1 etc. and hero template tweaks

--- web/themes/custom/drupal_celebrations/tailwind.config.js
+++ web/themes/custom/drupal_celebrations/tailwind.config.js
@@ -1,51 +1,73 @@
-const plugin = require('tailwindcss/plugin');
+const plugin = require("tailwindcss/plugin");
 
 module.exports = {
   theme: {
     fontFamily: {
-      sans: ['metropolis', 'sans-serif'],
-      serif: ['Lora', 'georgia', 'serif'],
-      mono: ['Menlo', 'Monaco', 'Consolas', '"Liberation Mono"', '"Courier New"', 'monospace'],
+      sans: ["metropolis", "sans-serif"],
+      serif: ["Lora", "georgia", "serif"],
+      mono: [
+        "Menlo",
+        "Monaco",
+        "Consolas",
+        '"Liberation Mono"',
+        '"Courier New"',
+        "monospace"
+      ]
     },
     extend: {
+      headings: {
+        h1: {
+          sm: "xl",
+          md: "2xl",
+          lg: "2xl",
+          xl: "4xl",
+        }
+      },
       minWidth: {
-        '9': '9.35rem',
-        '16': '16rem',
-        '24': '24rem',
-        '32': '32rem',
-        '38': '38rem',
+        "9": "9.35rem",
+        "16": "16rem",
+        "24": "24rem",
+        "32": "32rem",
+        "38": "38rem"
       },
       maxWidth: {
-        '7xl': '90rem',
+        "7xl": "90rem"
       },
       maxHeight: {
-        '75': '18.75rem',
+        "75": "18.75rem"
       },
       padding: {
         // 74rem = 90rem (7xl) - 16rem (px-32)
-        'center-two-columns': 'calc(calc(100vw - 74rem) / 2)',
+        "center-two-columns": "calc(calc(100vw - 74rem) / 2)"
       },
+      height: theme => ({
+        "screen/1": "75vh",
+        "screen/2": "50vh",
+        "screen/3": "calc(100vh / 3)",
+        "screen/4": "calc(100vh / 4)",
+        "screen/5": "calc(100vh / 5)",
+      }),
       spacing: {
-        '7': '1.75rem',
-        '11': '2.75rem',
-        '13': '3.25rem',
-        '14': '3.5rem',
-        '15': '3.75rem',
-        '17': '4.25rem',
-        '18': '4.5rem',
-        '19': '4.75rem',
-        '30': '7.5rem',
-        '45': '11.25rem',
-        '74': '18.5rem',
-        '88': '22rem',
+        "7": "1.75rem",
+        "11": "2.75rem",
+        "13": "3.25rem",
+        "14": "3.5rem",
+        "15": "3.75rem",
+        "17": "4.25rem",
+        "18": "4.5rem",
+        "19": "4.75rem",
+        "30": "7.5rem",
+        "45": "11.25rem",
+        "74": "18.5rem",
+        "88": "22rem"
       },
       inset: {
-        init: 'initial',
+        init: "initial"
       },
       letterSpacing: {
-        'wide-light': '0.01em',
-      },
-    },
+        "wide-light": "0.01em"
+      }
+    }
   },
   variants: {
     // https://tailwindcss.com/docs/pseudo-class-variants/#default-variants-reference
@@ -63,30 +85,31 @@ module.exports = {
     //   'active',
     //   'visited',
     //   'disabled',
-    padding: ['first', 'last', 'responsive'],
-    margin: ['first', 'last', 'responsive'],
-    borderWidth: ['first', 'last', 'responsive'],
-    backgroundColor: ['hover', 'focus', 'active', 'responsive'],
-    textColor: ['hover', 'focus', 'active'],
-    width: ['responsive'],
-    height: ['responsive'],
+    padding: ["first", "last", "responsive"],
+    margin: ["first", "last", "responsive"],
+    borderWidth: ["first", "last", "responsive"],
+    backgroundColor: ["hover", "focus", "active", "responsive"],
+    textColor: ["hover", "focus", "active"],
+    width: ["responsive"],
+    height: ["responsive"]
   },
   plugins: [
-    plugin(function ({
-      addBase,
-      config
-    }) {
-      addBase({
-        h1: {
-          fontSize: config('theme.fontSize.2xl')
-        },
-        h2: {
-          fontSize: config('theme.fontSize.xl')
-        },
-        h3: {
-          fontSize: config('theme.fontSize.lg')
-        },
-      })
-    }),
-  ],
+    plugin(function({ addBase, theme }) {
+      Object.entries(theme("headings", {})).forEach(([h, sizes]) => {
+        Object.entries(sizes).forEach(([bp, fs]) => {
+          addBase({
+            [`@screen ${bp}`]: {
+              [h]: {
+                fontSize: theme(`fontSize.${fs}`, fs)
+              }
+            }
+          });
+        });
+      });
+    })
+  ]
 };
+
+// If you want the default values without the users changes then you can import `tailwindcss/defaultConfig`.
+// It is the raw default config. This means some things will be functions to reference other values.
+// If you want the resolved values you can use `tailwindcss/resolveConfig`.
