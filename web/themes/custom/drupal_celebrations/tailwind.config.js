cfcbbca39bbac81cac3ed84b9bd06344b66427af
Dan Lemon <daniel.lemon@amazee.com>
2020-05-24 03:44:54 +0200

    Font tweaks

--- web/themes/custom/drupal_celebrations/tailwind.config.js
+++ web/themes/custom/drupal_celebrations/tailwind.config.js
@@ -1,5 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
-const { colors: { teal, orange, pink, ...colours } } = defaultTheme;
+const { colors } = defaultTheme;
 const plugin = require("tailwindcss/plugin");
 const reset = require('@benface/tailwindcss-reset');
 const typography = require('tailwindcss-typography');
@@ -7,7 +7,64 @@ const forms = require('@tailwindcss/custom-forms');
 
 module.exports = {
   theme: {
-    colors: colours,
+    colors: {
+      ...colors,
+      gray: {
+        '100': '#FBFCFD',
+        '200': '#F5F8F9',
+        '300': '#EFF3F6',
+        '400': '#E3EAEF',
+        '500': '#D7E1E8',
+        '600': '#C2CBD1',
+        '700': '#81878B',
+        '800': '#616568',
+        '900': '#414446',
+      },
+      red: {
+        '100': '#FCECE9',
+        '200': '#F8CFC7',
+        '300': '#F4B2A5',
+        '400': '#EB7962',
+        '500': '#E33F1E',
+        '600': '#CC391B',
+        '700': '#882612',
+        '800': '#661C0E',
+        '900': '#441309',
+      },
+      yellow: {
+        '100': '#FFFAEC',
+        '200': '#FFF2CF',
+        '300': '#FEEAB3',
+        '400': '#FEDA79',
+        '500': '#FDCA40',
+        '600': '#E4B63A',
+        '700': '#987926',
+        '800': '#725B1D',
+        '900': '#4C3D13',
+      },
+      green: {
+        '100': '#ECF6E8',
+        '200': '#CFE8C6',
+        '300': '#B2DAA4',
+        '400': '#79BE60',
+        '500': '#3FA21C',
+        '600': '#399219',
+        '700': '#266111',
+        '800': '#1C490D',
+        '900': '#133108',
+      },
+      blue: {
+        '100': '#EEF7FD',
+        '200': '#D4EBFA',
+        '300': '#BADFF7',
+        '400': '#87C8F1',
+        '500': '#53B0EB',
+        '600': '#4B9ED4',
+        '700': '#326A8D',
+        '800': '#254F6A',
+        '900': '#193547',
+      },
+    },
     fontFamily: {
       sans: ["metropolis", "sans-serif"],
       serif: ["Lora", "georgia", "serif"],
@@ -27,6 +84,179 @@ module.exports = {
         },
       },
     }),
+    typography: {
+      h1: {
+        fontSize: defaultTheme.fontSize['4xl'],
+        fontWeight: defaultTheme.fontWeight.bold,
+        color: defaultTheme.colors.gray[900],
+        lineHeight: defaultTheme.lineHeight.none
+      },
+      'h1 + *': {
+        marginTop: defaultTheme.spacing[8],
+      },
+      h2: {
+        fontSize: defaultTheme.fontSize['2xl'],
+        fontWeight: defaultTheme.fontWeight.medium,
+        color: defaultTheme.colors.gray[900],
+        lineHeight: defaultTheme.lineHeight.tight
+      },
+      '* + h2': {
+        marginTop: defaultTheme.spacing[8],
+      },
+      'h2 + *': {
+        marginTop: defaultTheme.spacing[4],
+      },
+      h3: {
+        fontSize: defaultTheme.fontSize.lg,
+        fontWeight: defaultTheme.fontWeight.medium,
+        color: defaultTheme.colors.gray[900],
+        lineHeight: defaultTheme.lineHeight.tight
+      },
+      '* + h3': {
+        marginTop: defaultTheme.spacing[6],
+      },
+      'h2 + h3': {
+        marginTop: defaultTheme.spacing[4],
+      },
+      'h3 + *': {
+        marginTop: defaultTheme.spacing[2],
+      },
+      h4: {
+        fontSize: defaultTheme.fontSize.base,
+        fontWeight: defaultTheme.fontWeight.semibold,
+        color: defaultTheme.colors.gray[900],
+        lineHeight: defaultTheme.lineHeight.normal
+      },
+      '* + h4': {
+        marginTop: defaultTheme.spacing[6],
+      },
+      'h3 + h4': {
+        marginTop: defaultTheme.spacing[2],
+      },
+      'h4 + *': {
+        marginTop: defaultTheme.spacing[2],
+      },
+      p: {
+        fontSize: defaultTheme.fontSize['sm'],
+        fontWeight: defaultTheme.fontWeight.normal,
+        color: defaultTheme.colors.gray[800],
+        lineHeight: defaultTheme.lineHeight.relaxed,
+      },
+      'p + p': {
+        marginTop: defaultTheme.spacing[4]
+      },
+      strong: {
+        fontWeight: defaultTheme.fontWeight.semibold,
+        color: defaultTheme.colors.gray[900],
+      },
+      a: {
+        fontWeight: defaultTheme.fontWeight.medium,
+        color: defaultTheme.colors.blue[700],
+      },
+      'a:hover': {
+        textDecoration: 'underline',
+      },
+      code: {
+        backgroundColor: defaultTheme.colors.gray[200],
+        fontSize: '.875em', // Use `em` so change is relative to current font size
+        paddingLeft: defaultTheme.spacing[1],
+        paddingRight: defaultTheme.spacing[1],
+      },
+      'img': {
+        marginTop: defaultTheme.spacing[8],
+        marginBottom: defaultTheme.spacing[8],
+      },
+      ol: {
+        listStyleType: 'decimal',
+        paddingLeft: defaultTheme.spacing[5]
+      },
+      '* + ol': {
+        marginTop: defaultTheme.spacing[4],
+      },
+      'ol + *': {
+        marginTop: defaultTheme.spacing[4],
+      },
+      'li ol': {
+        marginTop: defaultTheme.spacing[2],
+      },
+      ul: {
+        listStyleType: 'disc',
+        paddingLeft: defaultTheme.spacing[5]
+      },
+      '* + ul': {
+        marginTop: defaultTheme.spacing[4],
+      },
+      'ul + *': {
+        marginTop: defaultTheme.spacing[4],
+      },
+      'li ul': {
+        marginTop: defaultTheme.spacing[2],
+      },
+      li: {
+        fontSize: defaultTheme.fontSize.sm,
+        fontWeight: defaultTheme.fontWeight.normal,
+        color: defaultTheme.colors.gray[800],
+        lineHeight: defaultTheme.lineHeight.relaxed,
+      },
+      'li + li': {
+        marginTop: defaultTheme.spacing[2],
+      },
+      'li p': {
+        marginTop: defaultTheme.spacing[4],
+      },
+      'li p + p': {
+        marginTop: defaultTheme.spacing[2],
+      },
+      'li:first-child p:first-child': {
+        marginTop: defaultTheme.spacing[2],
+      },
+      blockquote: {
+        fontStyle: 'italic',
+        borderLeftWidth: defaultTheme.borderWidth[4],
+        borderLeftStyle: 'solid',
+        borderLeftColor: defaultTheme.colors.gray[300],
+        paddingLeft: defaultTheme.spacing[4]
+      },
+      '* + blockquote': {
+        marginTop: defaultTheme.spacing[4],
+      },
+      'blockquote + *': {
+        marginTop: defaultTheme.spacing[4],
+      },
+      pre: {
+        borderRadius: defaultTheme.borderRadius.md,
+        backgroundColor: defaultTheme.colors.gray[800],
+        color: defaultTheme.colors.white,
+        fontSize: '.875em', // Use `em` so change is relative to current font size
+        paddingTop: defaultTheme.spacing[3],
+        paddingRight: defaultTheme.spacing[4],
+        paddingBottom: defaultTheme.spacing[3],
+        paddingLeft: defaultTheme.spacing[4],
+        overflowX: "auto",
+        '-webkit-font-smoothing': 'subpixel-antialiased',
+        '-moz-osx-font-smoothing': 'auto',
+      },
+      '* + pre': {
+        marginTop: defaultTheme.spacing[4],
+      },
+      'pre + *': {
+        marginTop: defaultTheme.spacing[4],
+      },
+      'pre code': {
+        backgroundColor: defaultTheme.colors.transparent,
+        color: 'currentColor',
+        fontSize: '.875em', // Use `em` so change is relative to current font size
+        padding: 0,
+        '-webkit-font-smoothing': 'subpixel-antialiased',
+        '-moz-osx-font-smoothing': 'auto',
+      },
+      hr: {
+        borderTopWidth: defaultTheme.borderWidth.default,
+        borderColor: defaultTheme.colors.gray[500],
+        marginTop: defaultTheme.spacing[10],
+        marginBottom: defaultTheme.spacing[10],
+      }
+    },
     extend: {
       headings: {
         h1: {
@@ -141,6 +371,11 @@ module.exports = {
     forms,
     plugin(function({ addBase, theme }) {
       Object.entries(theme("headings", {})).forEach(([h, sizes]) => {
+        addBase({
+          [h]: {
+            fontSize: theme(`fontSize.${sizes.sm}`)
+          },
+        });
         Object.entries(sizes).forEach(([bp, fs]) => {
           addBase({
             [`@screen ${bp}`]: {
